7e8132e5c3494619412498615ec4ed04d647dc2e
RuvalcabaDev <ruvsolis@gmail.com>
2021-09-20 21:20:29 -0500

    hot fix

--- enviar.php
+++ enviar.php
@@ -1,6 +1,6 @@
 <?php
 
-$nombreWeb = "desde tupagina.com";
+$nombreWeb = "desde ruvalcaba.dev";
 
 if(isset($_POST['submit'])) { 
     $nombre = $_POST['nombre'];
@@ -9,7 +9,7 @@ if(isset($_POST['submit'])) {
     $cuerpoMensaje = $_POST['mensaje'];
     
     
-    $email_to = "example@example.com";
+    $email_to = "salvador@ruvalcaba.dev";
     $email_subject = "Contactado por $nombre $nombreWeb";
     $mensaje = "Mensaje enviado $nombreWeb \n";
     $mensaje.= "Nombre: $nombre \n";
